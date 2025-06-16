1763a5b3812843fc82a137c3d5c91ae196042192
Akashkapoor0 <akashkapoor.work@gmail.com>
2025-06-16 10:44:42 +0000

    mongoDB

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ dotenv.config()
 import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
 import helmet from 'helmet'
+import connectDB from "./config/connectDB.js"
 
 const app = express()
 app.use(cors({
@@ -26,6 +27,9 @@ app.get("/",(req,res)=>{
     })
 })
 
-app.listen(PORT,()=>{
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
     console.log("Server is Running",PORT)
+    })
 })
+
